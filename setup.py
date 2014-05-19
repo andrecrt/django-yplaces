f34d5c48a4b29b1fdc349213655437f473bd83c4
André Tavares <mail@andretavares.com>
2014-05-19 23:39:26 +0100

    Solved minor issue with ajax loader image. Updated PyPi package version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-yplaces',
-    version='0.2.8',
+    version='0.2.8.1',
     packages=['yplaces'],
     include_package_data=True,
     license='BSD License',  # example license
